8da3daf2920d51738048dc05e7fed0db32626051
NewtC <newtc12@gmail.com>
2020-11-26 23:33:57 -0800

    Updated the JSON converter with Active variable and seperated the profiles into their own dictionary.

--- Vote/json_converter.py
+++ Vote/json_converter.py
@@ -2,19 +2,24 @@ from json import dump
 from os import listdir, path, chmod
 
 vote_dir = '..\\..\\Twitch\\Votes\\'
-json_file = {}
+
+# get the currently active profile and record it in the json
+with open(path.join(vote_dir, "active.txt"), "r") as reader:
+    active = reader.read()
+json_file = {
+    "Active Profile": active,
+    "Profiles": {}}
 for each in listdir(vote_dir):
-    json_file[each] = {}
     if path.isdir(path.join(vote_dir, each)):
+        json_file["Profiles"][each] = {}
         for sub_file in listdir(path.join(vote_dir, each)):
             vote_file_dir = path.join(vote_dir, each, sub_file)
             name = sub_file.split('.')[0]
-            print(each)
 
             if name != "votes":
                 with open(vote_file_dir, 'r') as logpile:
                     # [vote_value, votes_list, suggestion(contributor_name),length_of_game, last_added]
-                    json_file[each][name] = {"vote value": int(logpile.read()),
+                    json_file["Profiles"][each][name] = {"vote value": int(logpile.read()),
                                        "votes list": {},
                                        "contributor": "",
                                        "length of game": 0,
