692c87b31ad476ef16aed184b5e35da9380c5217
NewtC <newtc12@gmail.com>
2021-01-10 13:05:32 -0800

    Reworded vote[index] to the correct wording.

--- Vote/Vote_StreamlabsSystem.py
+++ Vote/Vote_StreamlabsSystem.py
@@ -769,8 +769,10 @@ def decay():
     seconds_in_a_day = 86400
     vote_data = get_vote_data()
     for vote in vote_data["Profiles"][get_active_profile()].keys():
-        if time.time() - seconds_in_a_day * int(MySet.Decay_Days) > int(vote["last added"]):
-            vote["vote value"] = int(vote["vote value"]) - int(MySet.Decay_Amount)
-            if vote["vote value"] < 0:
-                vote["vote value"] = 0
+        if time.time() - seconds_in_a_day * int(MySet.Decay_Days) > \
+                vote_data["Profiles"][get_active_profile()][vote]["last added"]:
+            vote["vote value"] = vote_data["Profiles"][get_active_profile()][vote]["last added"]\
+                                 - int(MySet.Decay_Amount)
+            if vote_data["Profiles"][get_active_profile()][vote]["last added"] < 0:
+                vote_data["Profiles"][get_active_profile()][vote]["last added"] = 0
     update_vote_data(vote_data)
